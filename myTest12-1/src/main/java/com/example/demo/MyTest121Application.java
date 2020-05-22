457a418bda4e87734fec5a0fef19f7d27f594913
susu04-kor <suin0304_sw@google.com>
2020-05-22 18:49:09 +0900

    커밋 테스트

---
+++ myTest12-1/src/main/java/com/example/demo/MyTest121Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyTest121Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyTest121Application.class, args);
+	}
+
+}
